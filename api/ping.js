3f45cf9ed81b6ab81bfbc1ab80e2aae003e1f072
sandeep-megaska <sandeep.lalu@gmail.com>
2025-10-15 17:00:06 +0300

    Delete api directory

--- api/ping.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  const origin = req.headers.origin || req.headers.Origin || "*";
-  res.setHeader("Access-Control-Allow-Origin", origin);
-  res.setHeader("Vary", "Origin");
-  res.status(200).json({ ok: true, ts: Date.now() });
-};
