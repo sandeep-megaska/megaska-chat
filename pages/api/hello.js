eb430e13b9d919327a7ccf768bdd25546494e2fc
sandeep-megaska <sandeep.lalu@gmail.com>
2025-10-16 10:07:41 +0300

    Use only Next.js Pages API for /api/hello and /api/ping

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,4 @@
-﻿export default function handler(req, res) {
+﻿// pages/api/hello.js
+export default function handler(req, res) {
   res.status(200).send("hello");
 }
--- pages/api/ping.js
+++ pages/api/ping.js
@@ -1,4 +1,5 @@
-﻿export default function handler(req, res) {
+﻿// pages/api/ping.js
+export default function handler(req, res) {
   try { res.status(200).send("ok"); }
   catch (e) { console.error("PING_FATAL:", e); res.status(500).send("fail"); }
 }
